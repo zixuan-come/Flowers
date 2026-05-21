e84d8a1c0db4f0fd05f87957acbe3e2a230cee16
zixuan-come <zixuan0105@outlook.com>
2026-05-21 08:29:37 +0800

    工具小合集

--- myTimer.py
+++ myTimer.py
@@ -1,23 +1,51 @@
 import time
 
+def my_timer(unit="s"):
+    def decorate(func):
+        def wrapper(*args, **kwargs):
+            start = time.time()
+            function = func(*args, **kwargs)
+            print(f"消耗时间：{time.time() - start}{unit}")
+            return function
+        return wrapper
+    return decorate
 
 
-def my_timer(func):
-    def wrapper():
-        start_time = time.time()
-        result = func() # 执行传进来的函数
-        print(f"耗时：{time.time()-start_time:.2f}s")
-        return result
-    return wrapper
+@my_timer(unit="s")
+def add(a, b):
+    return a+b
 
-@my_timer
-def slow_function():
-    time.sleep(1)
-    return "done"
+
+ret = add(1, 2)
+print(ret)
 
 
 
-slow_function = my_timer(slow_function)
-slow_function()
 
 
+
+# import time
+#
+#
+# def my_timer(unit="s"):
+#     def decorator(func):
+#         def wrapper(*args, **kwargs):
+#             start_time = time.time()
+#             result = func(*args, **kwargs) # 执行传进来的函数
+#             elapsed = time.time()-start_time
+#             if unit == "ms":
+#                 print(f"耗时：{elapsed*1000:.2f}ms")
+#             else:
+#                 print(f"耗时：{elapsed :.2f}s")
+#             return result
+#         return wrapper
+#     return decorator
+#
+# @my_timer(unit="s")
+# def slow_function(*args, **kwargs):
+#     time.sleep(1)
+#     return "done"
+#
+#
+# slow_function()
+#
