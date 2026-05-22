e2208f26b30a3a9e160c6517bde13a8461260ba4
zixuan-come <zixuan0105@outlook.com>
2026-05-23 00:48:01 +0800

    控制器

---
+++ controller.py
@@ -0,0 +1,18 @@
+
+class MyController:
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+
+
+
+with MyController("test.txt", "w") as f:
+    f.write("hello")
+
